04379390d7c9a56517febfe57f0fcba58d9702a8
명윤 <100angru@gmail.com>
2018-01-29 22:15:25 +0900

    MY
    1/29 일부 수정. if status==4 에 들어가질 않음.. 이유를 모르겠음

--- test1.ino
+++ test1.ino
@@ -11,7 +11,7 @@ unsigned long pos = 0;    //현재 위치 변수.
 #include <SoftwareSerial.h>                        // 시리얼 통신에 필요한 라이브러리를 불러옵니다.
 SoftwareSerial BTSerial(2, 3);                        // 시리얼 통신핀을 설정합니다.
 // 이름을 BTSerial 로 설정하고, 2번 핀을 RX, 3번 핀을 TX로 설정합니다
-File myFile;
+File myFile, myFile2;
 int status = 0;   // 1은 파일 전송 시작. 3은 파일전송 끝. 2가 파일 읽어오기 시작.
 int num = 0;
 byte buffer[50];
@@ -57,7 +57,8 @@ void loop()
     // Serial.println("Here1");
     if (ch == 128 ) {   //파일 전송 시작을 알리는 프로토콜 인식
       status = 1;
-      myFile.write(0b0); //아무것도 없는 거 일단 한번 입력해봄.
+      byte myfilew = 0b00111111;
+      myFile.write(myfilew); //아무것도 없는 거 일단 한번 입력해봄.
       Serial.println("Here2");
     }
     while (status == 1) {   //파일 데이터 프로토콜 인식
@@ -75,11 +76,11 @@ void loop()
       }
       else if ( ((ch >> 6) & 0b11) == 0b01) { //파일 전송 끝을 알리는 프로토콜 인식
         status = 3;   //종료를 알리는 status
-        Serial.println("Here3");
         for (int i = 0; i < (num % 50); i++)
           myFile.write(buffer[i]);
 
         myFile.close();
+        Serial.println("Here3");
         break;
       }
       // Serial.println(num);
@@ -88,41 +89,46 @@ void loop()
   while (status == 2 || status == 3 || status == 4) //파일 전송 및 입력이 다 끝났다면...
   {
     if (status == 3) {
-      myFile = SD.open("test1.txt", FILE_READ);     //파일 여는 것을 한번만 실행하기 위해서.
+      Serial.println("Here4");
+      myFile2 = SD.open("test1.txt");     //파일 여는 것을 한번만 실행하기 위해서.
       status = 2;
       continue;
     }
-    if (myFile) {
+    if (myFile2) {
+      Serial.println("Here5");
       if (status == 2) { //임시 status 값. 처음 읽는거 확인.
         status = 4;
-        byte ch = myFile.read();
+        Serial.println("Here6");
+        byte ch = myFile2.read();
         Print(ch);
-        Serial.println(myFile.position());  //myFile.position() 함수가 현재 위치를 반환.
+        Serial.println(myFile2.position());  //myFile.position() 함수가 현재 위치를 반환.
         Serial.println(pos);                //맨 처음 부분이 1인듯. 원래 0부터 나올텐데 여기서는 0으로 돌아가질 않음.
       }
       if (status == 4) {  //한번 읽고 난뒤에 버튼을 누른다면..
-        if (digitalRead(whitebtn) == LOW && myFile.available()) {
+        Serial.println("Here7");
+        if (digitalRead(whitebtn) == LOW && myFile2.available()) {
           pos++;
-          myFile.seek(pos);
-          byte ch = myFile.read();
+          myFile2.seek(pos);
+          byte ch = myFile2.read();
           Print(ch);
           //Serial.println(num);
-          Serial.println(myFile.position());
+          Serial.println(myFile2.position());
           Serial.println(pos);
         }
-        if (digitalRead(redbtn) == LOW && myFile.available()) {
+        if (digitalRead(redbtn) == LOW && myFile2.available()) {
           pos--;
-          myFile.seek(pos);
-          byte ch = myFile.read();
+          myFile2.seek(pos);
+          byte ch = myFile2.read();
           Print(ch);
           //Serial.println(num);
-          Serial.println(myFile.position());
+          Serial.println(myFile2.position());
           Serial.println(pos);
         }
       }
     }
+    delay(175);
   }
-  delay(175);    //채터링 방지 이거를 파일 전송및 입력이 다끝난 후..
+      //채터링 방지 이거를 파일 전송및 입력이 다끝난 후..
   // 파일을 읽어 올 때 안에다가 넣어야 하지만 지금 실험중이기 때문에 밖에 꺼내놓음.
 }
 
